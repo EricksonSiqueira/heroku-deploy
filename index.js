42821abc25a6907716684539fced2194c9f5a95e
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-17 15:26:00 -0300

    refactor: utiliza h2 no index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send(`<h1>Executando na porta ${PORT}</h1>`))
+app.get('/', (req, res) => res.send(`<h2>Executando na porta ${PORT}</h2>`))
 
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
